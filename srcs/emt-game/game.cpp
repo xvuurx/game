85d3120a465d3775c6e1abb046933c6d6e5ceab4
FatalError <ripcode0@gmail.com>
2025-08-02 08:40:16 +0900

    up

---
+++ srcs/emt-game/game.cpp
@@ -0,0 +1,7 @@
+#include "game.h"
+
+const char *emt::get_name()
+{
+    std::string out("game");
+    return out.c_str();
+}
---
+++ srcs/emt-game/game.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <string>
+
+namespace emt
+{
+    const char* get_name();
+} // namespace emt
